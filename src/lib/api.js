7d4e7d20f2f9629725d72fa5a3316889f4ef63f0
kimochg <kimochg@outlook.com>
2017-02-24 19:08:21 +0800

    feat(api): get/add notes to firebase db

--- src/lib/api.js
+++ src/lib/api.js
@@ -11,6 +11,20 @@ const api = {
     const url = `https://api.github.com/users/${username}/repos`;
     return fetch(url)
       .then(res => res.json(), err => console.log(err))    
+  },
+  getNotes(username) {
+    username = username.toLowerCase().trim();
+    const url = `https://githubnotetaker-4c439.firebaseio.com/${username}.json`;
+    return fetch(url)
+      .then(res => res.json(), err => console.log(err))
+  },
+  addNote(username, note) {
+    username = username.toLowerCase().trim();
+    const url = `https://githubnotetaker-4c439.firebaseio.com/${username}.json`;
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(note)
+    }).then(res => res.json(), err => console.log(err))
   }
 }
 

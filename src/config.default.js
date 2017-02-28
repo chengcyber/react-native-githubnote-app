46fe24dfc16674a6c6080202bf2b789c9bc112d1
kimochg <kimochg@outlook.com>
2017-02-28 20:32:52 +0800

    feat(config): add config file examples for github auth

---
+++ src/config.default.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: LIU CHENG 
+ * @Date: 2017-02-27 22:02:12 
+ * @Last Modified by: LIU CHENG
+ * @Last Modified time: 2017-02-27 22:29:38
+ */
+
+/**
+ * Github account to OAuth graphql
+ */
+export const github = {
+  username: '',
+  password: '',
+  GITHUB_CLIENT_ID: 'your_github_client_id',
+  GITHUB_CLIENT_SECRET: 'your_github_client_secret',
+}
